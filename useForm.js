8667fdc5691ca33b4a65614628a26008331a6d8a
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-10-19 13:54:45 -0700

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+
+export const useForm = (initialForm = {}) => {
+
+    const [formState, setFormState] = useState(initialForm);
+
+    const onInputChange = ({target}) => {
+        const {name, value} = target;
+        //console.log({name, value});
+        setFormState({
+            ...formState,
+            // PROPIEDADES COMPUTADAS DE LOS OBJETOS
+            [name] : value
+        });
+    }
+
+    const onResetForm = () => {
+        setFormState(initialForm);
+    }
+
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm
+  }
+}
